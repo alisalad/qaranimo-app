9ce50e75a56575ec4db588672658fc76517b30f2
Ali Bashi <alisalad123@gmail.com>
2018-10-05 15:32:15 +1000

    layout styling

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
       <Link to="/">
-        <img src={require('../images/udub.svg')} width="30" />
+        <img src={require('../images/udub_logo.svg')} width="125" />
       </Link>
       <Link to="/home">Home</Link>
       <Link to="/about">About Us</Link>
