b405069e539c50922a100cc4f36c8cae74d70863
Ali Bashi <alisalad123@gmail.com>
2018-10-06 17:17:10 +1000

    components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-
+import Card from '../components/Card'
 const IndexPage = () => (
   <Layout>
     <div className="Hero">
@@ -13,6 +13,31 @@ const IndexPage = () => (
           libraries to get your site/product online
         </p>
         <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
+    <div className="Cards">
+      <h2>11 courses more comimg</h2>
+      <div className="CardGroup">
+        <Card
+          title="React for Designers"
+          text="12 sections"
+          image={require('../images/hero.jpg')}
+        />
+        <Card
+          title="Wordpress Traning"
+          text="15 sections"
+          image={require('../images/hero.jpg')}
+        />
+        <Card
+          title="AdobeXD Traning"
+          text="10 sections"
+          image={require('../images/hero.jpg')}
+        />
+        <Card
+          title="AdobeXD Traning"
+          text="10 sections"
+          image={require('../images/hero.jpg')}
+        />
       </div>
     </div>
   </Layout>
